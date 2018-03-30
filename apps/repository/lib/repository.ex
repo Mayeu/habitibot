ee39f7d6df015184245802a761a34a6036c9b729
Mayeu <m@mayeu.me>
2018-03-30 12:42:02 +0700

    feat(repo): add basic repository app based on Ecto

---
+++ apps/repository/lib/repository.ex
@@ -0,0 +1,3 @@
+defmodule Repository do
+  use Ecto.Repo, otp_app: :repository
+end
---
+++ apps/repository/priv/repository/migrations/20180330050915_create_user.exs
@@ -0,0 +1,7 @@
+defmodule Repository.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
