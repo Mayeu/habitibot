5c030fdfa50043d8b55fc9f5e41c9bff3e5c165b
Mayeu <m@mayeu.me>
2018-03-31 17:18:47 +0700

    feat: add the user schema in the repository

--- apps/repository/priv/repository/migrations/20180330050915_create_user.exs
+++ apps/repository/priv/repository/migrations/20180330050915_create_user.exs
@@ -2,6 +2,12 @@ defmodule Repository.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+      add(:user_id, :string)
+      add(:api_token, :string)
+      add(:username, :string)
+      add(:group_id, :string)
+      add(:quest_bot, :boolean)
+    end
   end
 end
