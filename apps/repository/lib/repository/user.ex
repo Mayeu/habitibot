9c5032cb43ccf563b7442b9be3c8af3dfbbe7f52
Mayeu <m@mayeu.me>
2018-03-31 17:18:47 +0700

    feat: add the user schema in the repository

---
+++ apps/repository/lib/repository/user.ex
@@ -0,0 +1,22 @@
+defmodule Repository.User do
+  use Ecto.Schema
+
+  alias Ecto
+
+  schema "users" do
+    field(:user_id, :string)
+    field(:api_token, :string)
+    field(:username, :string)
+    field(:group_id, :string)
+    field(:quest_bot, :boolean, default: false)
+  end
+
+  def changeset(user, params \\ %{}) do
+    accepted_fields = [:user_id, :api_token, :username, :group_id, :quest_bot]
+    required_fields = [:user_id, :api_token, :username, :quest_bot]
+
+    user
+    |> Changeset.cast(params, accepted_fields)
+    |> Changeset.validate_required(required_fields)
+  end
+end
