a4d2433daab3086726faacd3efdd4dfe271280a6
Mayeu <m@mayeu.me>
2018-04-02 19:06:33 +0700

    feat(login): user are redirected from the homepage if they are already login

--- apps/habitibot_web/lib/habitibot_web/controllers/page_controller.ex
+++ apps/habitibot_web/lib/habitibot_web/controllers/page_controller.ex
@@ -2,6 +2,24 @@ defmodule HabitibotWeb.PageController do
   use HabitibotWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+
+      conn ->
+        conn
+        |> redirect(to: setting_path(conn, :index))
+    end
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
