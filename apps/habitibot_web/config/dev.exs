6744d6209c9765b2538f9dc1b99c928c6795d697
Mayeu <m@mayeu.me>
2018-04-21 19:06:54 +0700

    fix: listen only to localhost and not on all interface

--- apps/habitibot_web/config/dev.exs
+++ apps/habitibot_web/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :habitibot_web, HabitibotWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4000, ip: {127, 0, 0, 1}],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
